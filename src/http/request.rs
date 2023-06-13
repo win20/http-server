2fe3e7eb72121912c3c03ce353bf6f82936e4e23
Win Barua <win.barua2@gmail.com>
2023-06-13 19:15:26 +0100

    Actually parse query string

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,6 @@
 use super::method::Method;
 use super::method::MethodError;
+use super::QueryString;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::Debug;
@@ -10,7 +11,7 @@ use std::str::Utf8Error;
 
 pub struct Request<'buf> {
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
     method: Method,
 }
 
@@ -37,7 +38,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
 
         let mut query_string = None;
         if let Some(i) = path.find('?') {
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(QueryString::from(&path[i + 1..]));
             path = &path[..i];
         }
 
