c7bec2331ff63f25d8c3cbd0da0e7dfd84585221
Win Barua <win.barua2@gmail.com>
2023-06-16 18:23:42 +0100

    Returning the response from the server

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,4 +1,11 @@
 use super::StatusCode;
+use std::io::Write;
+use std::net::TcpStream;
+use std::write;
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    io::Result as IoResult,
+};
 
 pub struct Response {
     status_code: StatusCode,
@@ -8,5 +15,20 @@ pub struct Response {
 impl Response {
     pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Response { status_code, body }
+    }
+
+    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+        let body = match &self.body {
+            Some(body) => body,
+            None => "",
+        };
+
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body,
+        )
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::convert::TryInto;
-use std::{io::Read, net::TcpListener};
+use std::{io::Read, io::Write, net::TcpListener};
 
 pub struct Server {
     addr: String,
@@ -25,11 +25,22 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1>IT WORKS</h1>".to_string()),
+                                    )
                                 }
-                                Err(e) => println!("Failed to parse the request: {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse the request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
